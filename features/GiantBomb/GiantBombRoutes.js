c10231cfbc78dac246313c76b2971d0dd4713397
JJ Crossman <jj.crossman@gmail.com>
2016-11-06 22:34:08 -0600

    cleaned up github repo

--- features/GiantBomb/GiantBombRoutes.js
+++
@@ -1,8 +0,0 @@
-const GiantBombCtrl = require( "./GiantBombCtrl" );
-
-module.exports = app => {
-  app.route( "/api/description/:query" )
-    .get( GiantBombCtrl.queryGiantBombForDescription );
-  app.route( "/api/video/:query" )
-    .get( GiantBombCtrl.queryGiantBombForVideos );
-};
